cff97a0131d6cfc476ed5b1d24ad51914ca7dc45
Wai Wong <daomen@gmail.com>
2025-02-05 20:31:38 +0100

    Fetch messages

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def send_messages():
   if entry.get():
     messager_col.insert_one({"text": entry.get()})
     entry.delete(0, tk.END)
+    fetch_messages()
 
 send_button = tk.Button(root, text="Send", command=send_messages)
 send_button.pack(pady=5)
@@ -24,4 +25,10 @@ send_button.pack(pady=5)
 messages_label = tk.Label(root, text="Messages:\n", justify="left")
 messages_label.pack(pady=5)
 
+def fetch_messages():
+  messages = messager_col.find().sort("_id", 1)
+  messages_label.config(text="Messages:\n" + "\n".join(f"-{m['text']}" for m in messages))
+  root.after(2000, fetch_messages)
+
+fetch_messages()
 root.mainloop()
