6150f1e5740ce2c39de05e05f919f8ab7d7adb70
Wai Wong <daomen@gmail.com>
2025-02-05 20:17:46 +0100

    Added a send button

--- app.py
+++ app.py
@@ -13,5 +13,15 @@ root.title("PyChat")
 entry = tk.Entry(root, width=50)
 entry.pack(pady=5)
 
+def send_messages():
+  if entry.get():
+    messager_col.insert_one({"text": entry.get()})
+    entry.delete(0, tk.END)
+
+send_button = tk.Button(root, text="Send", command=send_messages)
+send_button.pack(pady=5)
+
+messages_label = tk.Label(root, text="Messages:\n", justify="left")
+messages_label.pack(pady=5)
 
 root.mainloop()
